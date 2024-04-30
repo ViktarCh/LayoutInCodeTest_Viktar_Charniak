d42ddccfe92a0e87143f020cfc0785c4a72f97af
Viktar Charniak <moodooosgrodno@gmail.com>
2024-04-29 22:21:17 -0400

    Initial Commit

---
+++ LayoutInCodeTest_ViktarCharniak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutInCodeTest_ViktarCharniak
+//
+//  Created by Viktar Charniak on 4/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
